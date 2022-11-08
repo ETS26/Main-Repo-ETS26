0f5d0188a573fd1cc82e9b59dec5897a397f9fad
Engin Taha Sümer <115226745+ETS26@users.noreply.github.com>
2022-11-08 20:09:25 +0300

    Update and rename example6.java to w5.6.java

--- w5.6.java
+++ w5.6.java
@@ -17,18 +17,18 @@ public class example6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+     Scanner input = new Scanner(System.in);
      System.out.println("lütfen satır sayısını giriniz: ");
      int row = input.nextInt();
-         for(int i=1;i<=row;i++){
+     for(int i=1;i<=row;i++){
             
-            for(int j=1;j<=i;j++){
-               
-                System.out.print("*");
-            }
+     for(int j=1;j<=i;j++){
+     System.out.print("*");      
+     }        
+     System.out.println();      
             
-            System.out.println();
-    }
-    
-}
+    }    
+   
+   }
+
 }
