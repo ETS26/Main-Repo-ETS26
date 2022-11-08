7f59ed20ed04af5fd01bb896fb8447bf36c9e4c8
ETS26 <115226745+ETS26@users.noreply.github.com>
2022-11-08 19:27:03 +0300

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author engin
+ */
+import java.util.Scanner;
+public class example1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //konsoldan deger almak için tanımladık
+        System.out.println("Lütfen km cinsinden deger giriniz: ");
+        int km = input.nextInt();
+        int metre = km*1000;
+         System.out.println("Hesaplanan metre degeri: "+metre);
+         
+        
+        
+        
+    }
+    
+}
